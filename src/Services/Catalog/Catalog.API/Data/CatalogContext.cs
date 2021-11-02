957a129927a0739341b25dec085037d38d52dee0
phong tran <phong.tran@evizi.com>
2021-11-02 18:04:45 +0700

    - Add catalog project - Add mongo db - Add docker compose

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("DatabaseSettings:DatabaseName");
+            Products = database.GetCollection<Product>("Products");
+            CatalogContextSeed.SeedData(Products);
+
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string Id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryId);
+        Task CreateProduct(Product model);
+        Task<bool> UpdateProduct(Product model);
+        Task<bool> DeleteProduct(string productId);
+    }
+}
